f77ecf58fdcc7496439dc7dc2778ece6e4a54a16
ShahabMat <cse_1812020090@lus.ac.bd>
2023-09-24 15:56:36 +0600

    completed the project

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -11,6 +11,7 @@ const Home = () => {
     const [remaining, setRemaining] = useState(0);
     const [totalCost, setTotalCost] = useState(0)
 
+    const budget = 40000;
 
     useEffect(() => {
         fetch('data.json')
@@ -30,20 +31,24 @@ const Home = () => {
             actors.forEach(item => {
                 count = count + item.salary;
             })
-            const totalRemaining = 20000 - count;
-            setRemaining(totalRemaining)
-            setTotalCost(count)    
+            const totalRemaining = budget - count;
 
-            const selectActor = [...actors, actor];
-            setActors(selectActor);
+            if (count > budget) {
+                return alert('Yours Sufficient Balance!!')
+            } else {
+                setTotalCost(count)
+                setRemaining(totalRemaining)
+                const selectActor = [...actors, actor];
+                setActors(selectActor);
+            }
         }
 
     }
 
     return (
-        <div className="container mx-auto">
+        <div className="container px-5 mx-auto">
             <div className="  md:flex  gap-5 my-10">
-                <div className="w-2/3 ">
+                <div className="md:w-2/3 ">
                     <h2 className="text-2xl">Home Area</h2>
                     <div className='grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10  ' >
                         {
@@ -57,7 +62,7 @@ const Home = () => {
                     </div>
                 </div>
 
-                <div className="w-1/3">
+                <div className="md:w-1/3 mt-10 md:mt-0">
                     <Cart actors={actors} remaining={remaining} totalCost={totalCost} ></Cart>
                 </div>
             </div>
